e0c5461d0c82c0c1a6a14a55db602367ee4f6bef
Karlruutel <karl.ruutel@ametikool.edu.ee>
2022-10-11 14:05:30 +0300

    commit

---
+++ yl2.py
@@ -0,0 +1,11 @@
+PI = 3.14
+
+Raadius = float(input(' Palun Sisestage ringi raadius: '))
+ 
+Ümbermõõt = 2 * PI * Raadius
+
+Pindala = PI * Raadius * Raadius
+ 
+print(" Ringi ümbermõõt = %.2f" %Ümbermõõt)
+
+print(" Ringi pindala = %.2f" %Pindala)
