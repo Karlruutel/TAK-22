98ab2b749e295ecf518bccff1540f500426e955c
Karlruutel <karlruutel@ametikool.edu.ee>
2022-10-20 15:25:23 +0300

    .3-6

---
+++ yl4.py
@@ -0,0 +1,7 @@
+a = input( 'a:' )
+b = input( 'b:' )
+
+if a < b:
+    print('a on miinimum')
+else:
+    print('b on miinimum')
---
+++ yl6.py
@@ -0,0 +1,13 @@
+a = int(input( 'a:' ))
+b = int(input( 'b:' ))
+c = int(input( 'c:' ))
+
+if a > b and a > c:
+    print('a on max',a)
+elif b > c:
+    print('b on max',b)
+else:
+    print('c on max',c)
+
+
+
